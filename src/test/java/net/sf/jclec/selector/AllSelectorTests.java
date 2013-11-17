8629fa6154bdf6a693b7d6be217b5e1828dfc16f
IvanSteven <IvanSteven@192.168.1.232>
2013-11-17 00:36:59 +0000

    My first commit!
    Signed-off-by: IvanSteven <IvanSteven@192.168.1.232>

---
+++ src/test/java/net/sf/jclec/selector/AllSelectorTests.java
@@ -0,0 +1,21 @@
+package net.sf.jclec.selector;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllSelectorTests 
+{
+	public static Test suite() {
+		TestSuite suite = new TestSuite(AllSelectorTests.class.getName());
+		//$JUnit-BEGIN$
+		suite.addTestSuite(DisruptiveSelectorTest.class);
+		suite.addTestSuite(SigmaScalingSelectorTest.class);
+		suite.addTestSuite(StochasticUniversalSamplingTest.class);
+		suite.addTestSuite(HierarchicalSelectorTest.class);
+		suite.addTestSuite(NonLinearHierarchicalSelectorTest.class);
+		suite.addTestSuite(StochasticRemainingSelectorTest.class);
+		//$JUnit-END$
+		return suite;
+	}
+
+}
